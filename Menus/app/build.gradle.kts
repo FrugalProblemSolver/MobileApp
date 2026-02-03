ba6b7813abfd232b89c206ed875d227583b82a81
Shyam-3 <shyamsa@student.tce.edu>
2026-02-03 16:00:42 +0530

    feat: initial commit

--- Menus/app/build.gradle.kts
+++ Menus/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.menus"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.menus"
--- Menus/app/src/main/java/com/example/menus/MainActivity.kt
+++ Menus/app/src/main/java/com/example/menus/MainActivity.kt
@@ -1,16 +1,54 @@
 package com.example.menus
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var supportActionBar: Toolbar
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.about_us ->{
+                val indent = Intent(this,AboutUsActivity::class.java)
+                startActivity(indent)
+                return true
+            }
+            R.id.members ->{
+                val intent = Intent(this,MembersActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.description->{
+                val intent = Intent(this,DescriptionActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
